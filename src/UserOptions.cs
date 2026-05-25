c470bcdf3bcd312a3e906d3e43f935ddb7259dd5
AaronCasildo <aacasildor@gmail.com>
2026-05-24 23:30:20 -0700

    feat: add useroptions class

---
+++ src/UserOptions.cs
@@ -0,0 +1,8 @@
+namespace Asciify;
+
+public class UserOptions
+{
+    public int With { get; set; } = 100;
+    public bool Invert { get; set; } = false;
+    public bool Color { get; set; } = false;
+}
