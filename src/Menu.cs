5ecb4acdbf077d7c61d7e7e41c2a89b2226d66f4
AaronCasildo <aacasildor@gmail.com>
2026-05-25 22:01:49 -0700

    chore: migrate renderize function to dedicated code

--- src/Menu.cs
+++ src/Menu.cs
@@ -57,40 +57,7 @@ public static class Menu
             .SpinnerStyle(Style.Parse("green"))
             .Start("Processing image...", ctx =>
             {
-                Renderize(useroptions, filePath);
+                Render.Renderize(useroptions, filePath);
             });
-    }
-
-    public static void Renderize(UserOptions options, string filePath)
-    {
-        //Load the image using SkiaSharp.
-        using var bitmap = SKBitmap.Decode(filePath);
-
-        //Redimention the iamge based on the user specified width while maintaining aspect ratio.
-        int newHeight =  (int)((float)bitmap.Height / bitmap.Width * options.With / 2);
-        var resized = new SKBitmap(new SKImageInfo(options.With, newHeight));
-        bitmap.ScalePixels(resized, SKSamplingOptions.Default);
-
-        // Defo need upgrading this to use color if the user wants it, but for now just render the ASCII art in grayscale.
-        // Map brightness to ASCII characters. The string of characters is ordered from darkest to lightest.
-        // The brightness calculation uses the luminosity method, which weights the RGB channels according to human perception.
-        // Its very basic and ugly for now but it works, and I can always improve it later (tomorrow lol)
-        
-        const string chars = " .,:;i1tfLCG08@";
-            var sb = new System.Text.StringBuilder();
-
-            for (int y = 0; y < resized.Height; y++)
-            {
-                for (int x = 0; x < resized.Width; x++)
-                {
-                    var pixel = resized.GetPixel(x, y);
-                    float brightness = (pixel.Red * 0.299f + pixel.Green * 0.587f + pixel.Blue * 0.114f) / 255f;
-                    if (options.Invert) brightness = 1f - brightness;
-                    int index = (int)(brightness * (chars.Length - 1));
-                    sb.Append(chars[index]);
-                }
-                sb.AppendLine();
-            }
-        AnsiConsole.Write(sb.ToString());
     }
 }
